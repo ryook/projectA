8ba33696490e3e1cab054ab7688a4370bba6a54f
MinotaroK <kyutyo.cow@gmail.com>
2015-10-22 22:29:45 +0900

    詰みファイル

--- tools/dmm_img/controller.js
+++ tools/dmm_img/controller.js
@@ -1,13 +1,22 @@
-$(window).load(function(){
-	$("#btn").on("click",(function(){
-		var clu = $("#text").val()
-		$("#img").empty()
-		$("#clu").empty()
-		$("#clu").append(clu)
-		data.forEach(function(d){
-			if(d["cluster"] == clu){
-				$("#img").append("<img src='"+d["img"]+"' />")
-			};
-		})
-	}));
-});
+var avApp = angular.module("avApp", ["ngResource"]);
+
+avApp.controller("mainCtrl", ["$scope", "$resource", function($scope, $resource){
+	var avData = $resource("data.json");
+	$scope.reviewData = avData.query();
+	/*var arrobj = {};
+
+	for(var i=0; i < reviewData.length; i++){
+		arrobj[reviewData[i]['cluster']] = reviewData[1];
+	}
+
+	newData = [];
+
+	for(var key in arrobj){
+		newData.push(arrobj[key]);
+	}*/
+
+//画像読み込み部分適当に書いただけ
+	$scope.click = function(){
+		$scope.getImage.src = d[image];
+	};
+}]);
