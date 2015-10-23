941d75b06f60c50e4711b166204450e4d5c34bc2
MinotaroK <kyutyo.cow@gmail.com>
2015-10-23 21:13:07 +0900

    ちょっと進んだ

--- tools/dmm_img/controller.js
+++ tools/dmm_img/controller.js
@@ -1,22 +1,50 @@
 var avApp = angular.module("avApp", ["ngResource"]);
 
 avApp.controller("mainCtrl", ["$scope", "$resource", function($scope, $resource){
-	var avData = $resource("data.json");
-	$scope.reviewData = avData.query();
-	/*var arrobj = {};
+	//var avData = $resource("data.json");
+	//clu_Data = avData.query();
+	var cluNum = data;
+	var numbclu = {};
 
-	for(var i=0; i < reviewData.length; i++){
-		arrobj[reviewData[i]['cluster']] = reviewData[1];
+//クラスタ番号のみ抽出
+	for(var i = 0; i < cluNum.length; i++){
+		numbclu[cluNum[i]["cluster"]] = cluNum[i];
 	}
 
-	newData = [];
+	numbClu =[];
 
-	for(var key in arrobj){
-		newData.push(arrobj[key]);
-	}*/
+	for(var key in numbclu){
+		numbClu.push(numbclu[key]);
+	}
+
+	onlynum = [];
+	for (key in numbClu){
+		onlynum.push(numbClu[key].cluster);
+	}
+
+	$scope.reviewData = onlynum;
+	$scope.selectedItem = null;
+	var imageList = [];
+	$scope.changeItem = function(){
+		var selected = $scope.selectedItem;
+		console.log(selected);
+		for (var i=0; i < cluNum.length; i++){
+			if (selected == cluNum[i]["cluster"]){
+				imageList.push(cluNum[i]["img"]);
+			}
+		}
+		console.log(imageList);
+		imageList = [];
+	};
+
+	console.log(onlynum);
+
+	var option = document.myForm.mySelect.options;
+
+
 
 //画像読み込み部分適当に書いただけ
 	$scope.click = function(){
-		$scope.getImage.src = d[image];
+		
 	};
 }]);
